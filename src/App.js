9e47043f99693e210fa54aa4d94e86f2d5526642
Chamath Jayarathne <chamathsocial@gmail.com>
2022-10-14 15:49:31 -0400

    Add CSS

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component{
         <SearchBox 
           onChangeHandler={onSearchChange} 
           placeholder="search monsters" 
-          className="search-box" 
+          className="monsters-search-box" 
         />
         
         <CardList monsters={filteredMonsters} />
